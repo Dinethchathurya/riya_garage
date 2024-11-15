3bf06f9e4b4e38743157ea80c5927b92f092780b
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2024-11-15 23:07:34 +0530

    refactor : second screen component

--- lib/View/Screens/HomeScreen.dart
+++ lib/View/Screens/HomeScreen.dart
@@ -16,6 +16,12 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        leading: Image.asset(
+          'assets/Riya-icon.png',
+          width: screenWidth * 0.8, // Increase the image size (adjust as needed)
+          height: screenHeight * 0.3, // Adjust height as needed
+          fit: BoxFit.cover, // Maintain the aspect ratio of the image
+        ),
         title: const Text("Home",
           style: TextStyle(
             color: Colors.white,
--- lib/View/Screens/SecondScreen.dart
+++ lib/View/Screens/SecondScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../AppColors.dart';
+import 'Components/SecondPageBoxWidget.dart';
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
@@ -24,57 +25,10 @@ class SecondScreen extends StatelessWidget {
         child: Column(
         children: [
           Center(
-            child: Container(
-              width: screenWidth * 0.8,
-              height: screenHeight * 0.3, // Keep container height the same
-              decoration: BoxDecoration(
-                color: AppColors.themeBlue,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5), // Shadow color with opacity
-                    spreadRadius: 4, // How much the shadow spreads
-                    blurRadius: 8,   // Softening the shadow (higher value means more blur)
-                    offset: const Offset(5, 5), // Offset in the x and y directions
-                  ),
-                ],
-              ),
-              margin: EdgeInsets.only(top: screenHeight * 0.08),
-              child: Center(
-                child: Image.asset(
-                  'assets/Maintenance-Repairs-icon.png',
-                  width: screenWidth * 0.8, // Increase the image size (adjust as needed)
-                  height: screenHeight * 0.3, // Adjust height as needed
-                  fit: BoxFit.cover, // Maintain the aspect ratio of the image
-                ),
-              ),
-            ),
+            child: SecondPageBoxWidget(screenWidth: screenWidth, screenHeight: screenHeight, margin: 0.08, image: 'assets/Maintenance-Repairs-icon.png',),
           ),
           Center(
-            child: Container(
-              width: screenWidth * 0.8,
-              height: screenHeight * 0.3,
-              decoration: BoxDecoration(
-                color: AppColors.themeBlue,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5), // Shadow color with opacity
-                    spreadRadius: 4, // How much the shadow spreads
-                    blurRadius: 8,   // Softening the shadow (higher value means more blur)
-                    offset: const Offset(5, 5), // Offset in the x and y directions
-                  ),
-                ],// Make it a circle
-              ),
-              margin: EdgeInsets.only(top: screenHeight * 0.1),
-              child: Center(
-                child: Image.asset('assets/SpareParts-BodyParts-icon.png',
-                  width: screenWidth * 0.8,
-                  height: screenHeight * 0.6,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
+            child: SecondPageBoxWidget(screenWidth: screenWidth, screenHeight: screenHeight, margin: 0.1, image: 'assets/SpareParts-BodyParts-icon.png')
           )
         ],
       ),
